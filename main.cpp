337085d39db62ed988a3e1a14966a5f292f26a58
George Whittington <80219357+GeorgeWhittington@users.noreply.github.com>
2023-10-27 15:14:36 +0100

    Rewrote in using the style of included nanugui examples

--- main.cpp
+++ main.cpp
@@ -8,28 +8,36 @@
 
 using namespace nanogui;
 
-int main() {
+class XStitchEditorApplication : public Screen {
+public:
+    XStitchEditorApplication() : Screen(Vector2i(1024, 768), "X Stitch Editor") {
+        inc_ref();
+
+        Window *window = new Window(this, "");
+        window->set_position(Vector2i(0, 0));
+        window->set_layout(new GroupLayout(5, 5, 0, 0));
+
+        Label *l = new Label(window, "MODULATION", "sans-bold");
+        l->set_font_size(10);
+        Slider *slider = new Slider(window);
+        slider->set_value(0.5f);
+        float modulation = 5.0f;
+        slider->set_callback([&modulation](float value) { modulation = value * 10.0f; });
+
+        perform_layout();
+    }
+};
+
+int main(int, char **) {
     nanogui::init();
 
-    nanogui::Screen app(Vector2i(1024, 768), "NanoGUI Test");
-
-    nanogui::Window window{&app, ""};
-    window.set_position(Vector2i(15, 15));
-    window.set_layout(new nanogui::GroupLayout(5, 5, 0, 0));
-
-    nanogui::Label *l = new nanogui::Label(&window, "MODULATION", "sans-bold");
-    l->set_font_size(10);
-    nanogui::Slider *slider = new nanogui::Slider(&window);
-    slider->set_value(0.5f);
-    float modulation = 5.0f;
-    slider->set_callback([&modulation](float value) { modulation = value * 10.0f; });
-
-    app.perform_layout();
-
-    app.draw_all();
-    app.set_visible(true);
-
-    nanogui::mainloop(1 / 60.f * 1000);
+    {
+        ref<XStitchEditorApplication> app = new XStitchEditorApplication();
+        app->dec_ref();
+        app->draw_all();
+        app->set_visible(true);
+        nanogui::mainloop(1 / 60.f * 1000);
+    }
 
     nanogui::shutdown();
     exit(EXIT_SUCCESS);
